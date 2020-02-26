bf0a664f2cdc1777749a0878de142da480766331
flyslime <randomthingyderp@gmail.com>
2020-02-26 17:41:53 +0100

    Start AKS.py

---
+++ Python/aks.py
@@ -0,0 +1,26 @@
+import math
+
+
+def isPerfectPower(n):
+    # Generate all divisors up to log2(n)
+    divisors = []
+    for x in range(2, int(math.log2(n)) + 1):
+        if n % x == 0:
+            divisors.append(x)
+
+    if len(divisors) == 0:
+        return False
+
+    i = 2
+    while pow(divisors[0], i) <= n:
+        for div in divisors:
+            if pow(div, i) == n:
+                return True
+        i += 1
+    return False
+
+
+# Temporary
+n = 20
+if isPerfectPower(n):
+    print("Composite")
