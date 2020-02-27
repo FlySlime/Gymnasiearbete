511963648f60a47e583cbabbd7e24a8e6b71ae1b
flyslime <randomthingyderp@gmail.com>
2020-02-27 12:34:31 +0100

    Better isPerfectPower()

--- Python/aks.py
+++ Python/aks.py
@@ -2,25 +2,14 @@ import math
 
 
 def isPerfectPower(n):
-    # Generate all divisors up to log2(n)
-    divisors = []
-    for x in range(2, math.ceil(math.log2(n)) + 1):
-        if n % x == 0:
-            divisors.append(x)
-
-    if len(divisors) == 0:
-        return False
-
-    i = 2
-    while pow(divisors[0], i) <= n:
-        for div in divisors:
-            if pow(div, i) == n:
-                return True
-        i += 1
+    # If n = a^b for integers a > 1 and b > 1, output composite.
+    for b in range(2, round(math.log2(n)) + 1):
+        a = pow(n, 1 / b)
+        if (a * 10) % 10 == 0:
+            return True
     return False
 
 
 # Temporary
-n = 36
 if isPerfectPower(n):
     print("Composite")
