4d8920c5df616f44f390ea2fdf361204ccb3240a
flyslime <randomthingyderp@gmail.com>
2020-02-27 13:10:31 +0100

    Add smallestR()

--- Python/aks.py
+++ Python/aks.py
@@ -1,6 +1,7 @@
 import math
 
 
+# Step 1
 def isPerfectPower(n):
     # If n = a^b for integers a > 1 and b > 1, output composite.
     for b in range(2, round(math.log2(n)) + 1):
@@ -10,6 +11,29 @@ def isPerfectPower(n):
     return False
 
 
+# Step 2
+def smallestR(n):
+    max_k = math.floor(pow(math.log2(n), 2))
+    next_r = True
+    r = 2
+    while next_r:
+        next_r = False
+        for k in range(1, max_k):
+            if next_r:
+                break
+
+            if pow(n, k, r) == 1:
+                next_r = 1
+            elif pow(n, k, r) == 0:
+                next_r = 0
+        r += 1
+    r -= 1
+    return r
+
+
 # Temporary
+n = 31
 if isPerfectPower(n):
     print("Composite")
+else:
+    print(smallestR(n))
