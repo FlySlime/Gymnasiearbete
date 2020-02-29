cb75c30df1ffdefb7f659b6cdbce79fc75a8c7d0
robinbrusbo <50497530+robinbrusbo@users.noreply.github.com>
2020-02-29 22:15:15 +0100

    Updates from Overleaf

--- Python/Theory_examples/n*n.py
+++
@@ -1,9 +0,0 @@
-def quadraticTime(N):
-    for number in range(N):
-        for number in range(N):
-            print(number)
-
-
-if __name__ == "__main__":
-    N = int(input())
-    quadraticTime(N)
