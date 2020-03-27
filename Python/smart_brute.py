2250eb48d27a6587c8b264aa499ddc8971904fe1
flyslime <randomthingyderp@gmail.com>
2020-03-27 11:26:48 +0100

    Fix bug in smartBrute function Must redo the results for smartBrute

--- Python/smart_brute.py
+++ Python/smart_brute.py
@@ -2,11 +2,13 @@ import math
 
 
 def smartBrute(n):
+    if n % 2 == 0:
+        return False
     if n == 1:
         return False
     if n == 2:
         return True
-    for x in range(3, math.isqrt(n), 2):
+    for x in range(3, math.isqrt(n) + 1, 2):
         if n % x == 0:
             return False
     return True
